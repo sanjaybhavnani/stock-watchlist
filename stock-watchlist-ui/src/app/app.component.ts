871cfc6b82f53b2a7f5a5a18deab38abde3972d0
Sanjay Bhavnani <sanjaylbhavnani@gmail.com>
2025-08-25 10:05:42 +0530

    clean up code

--- stock-watchlist-ui/src/app/app.component.ts
+++ stock-watchlist-ui/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { StocksService } from './shared/services/stocks.service';
+import { WebSocketService } from './shared/services/web-socket.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,17 @@ import { HeaderComponent } from './shared/components/header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stock-watchlist-ui';
+  private webSocketService = inject(WebSocketService);
+  ngOnInit() {
+    // Subscribe to your stocks feed here
+    // this.stocksSubscription = yourStocksService.subscribe(...);
+
+    window.addEventListener('beforeunload', this.cleanupStocksFeed);
+  }
+  private cleanupStocksFeed = () => {
+    console.log('Closing socket connection');
+    this.webSocketService.close();
+  };
 }
